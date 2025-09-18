bde54fd11b0fe123a8a9699a0a6192214e1dca07
mariam-lumin <mariam.ismail@luminsoft.net>
2025-09-18 17:09:46 +0300

    fixing localization

--- EnrollLite/features/FaceDetection/FaceDetectionViewController.swift
+++ EnrollLite/features/FaceDetection/FaceDetectionViewController.swift
@@ -102,7 +102,7 @@ class FaceDetectionViewController: UIViewController, AVCaptureVideoDataOutputSam
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+        super.viewWillAppear(animated)
         addDimmedLayerWithClearCircle()
         setupLabel()
     }
